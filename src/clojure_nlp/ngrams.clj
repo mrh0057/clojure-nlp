105d66c1bb3d9457c72f937423b4a6d961ecd640
Matt Hoyt <mrhoyt4@yahoo.com>
2011-11-26 04:37:24 -0600

    Added the ability to add ngrams to a document.

--- src/clojure_nlp/ngrams.clj
+++ src/clojure_nlp/ngrams.clj
@@ -12,3 +12,44 @@
   [seq number]
   (if (>= (count seq) number)
     (lazy-seq (cons (take number seq) (create-ngrams (rest seq) number)))))
+
+(defn n-grams-count
+  "Used to create n-grams with the specified numbers
+
+*numbers*
+  A seq of numbers for the ngrams to create. <br />
+*documents*
+  The documents to process with the ngrams <br />
+*returns*
+  A map of the ngrams"
+  [numbers documents]
+  (reduce (fn [counts number]
+             (reduce (fn [counts document]
+                       (reduce (fn [counts freqs]
+                                 (let [ngram (first freqs)]
+                                   (if-let [val  (counts ngram)]
+                                     (assoc counts ngram (+ val (second freqs)))
+                                     (assoc counts ngram (second freqs)))))
+                               counts (frequencies (create-ngrams document number))))
+                     counts documents)) {} numbers))
+
+(defn add-ngrams-to-document
+  "Used to add ngrams to the document.
+
+*numbers*
+  The number of ngrams to create. <br />
+*document*
+  The document to process. <br />
+*processed-document*
+  The processed-document to add the ngrams too. <br />
+*ngrams-count*
+  The ngrams map. <br />
+*return*
+  The processed document with the added ngrams"
+  [numbers document processed-document ngrams-count]
+  (reduce (fn [processed-document ngram]
+            (if (contains? ngrams-count (first ngram))
+              (assoc processed-document :counts
+                (assoc (:counts processed-document) (first ngram) (second ngram))
+                :number-of-words (+ (:number-of-words processed-document) (second ngram)))
+              processed-document)) processed-document (n-grams-count numbers (list document))))
--- test/clojure_nlp/test/ngrams.clj
+++ test/clojure_nlp/test/ngrams.clj
@@ -1,5 +1,6 @@
 (ns clojure-nlp.test.ngrams
   (:use clojure-nlp.ngrams
+        clojure-nlp.document
         clojure.test))
 
 (deftest create-ngrams-test
@@ -8,3 +9,33 @@
     (is (= correct (create-ngrams test-val 2)))
     (is (= nil (create-ngrams '() 2)))
     (is (= nil (create-ngrams ["a" "b"] 3)))))
+
+(deftest n-grams-count-test
+  (let [test-vals [["a" "b" "c"] ["b" "c"]]
+        vals {'("a" "b") 1
+              '("b" "c") 2}
+        vals2 {'("a" "b") 1
+               '("a" "b" "c") 1
+               '("b" "c") 2}]
+    (is (= (n-grams-count [2] test-vals) vals))
+    (is (= (n-grams-count [2 3] test-vals) vals2))
+    (is (= (n-grams-count [2 4] test-vals) vals))))
+
+(deftest add-ngrams-to-document-test
+  (let [document ["a" "b" "c" "b" "c"]
+        processed-document (make-processed-document "doc4"
+                                                    (hash-map "a" 1
+                                                              "b" 2
+                                                              "c" 2)
+                                                    5)
+        correct (make-processed-document "doc4"
+                                         (hash-map "a" 1
+                                                   "b" 2
+                                                   "c" 2
+                                                   '("b" "c") 2)
+                                         7)]
+    (is (= (add-ngrams-to-document [2 3 4 5]
+                                   document
+                                   processed-document
+                                   (hash-map '("b" "c") 10))
+           correct))))
